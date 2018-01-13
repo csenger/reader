794a56a773d55601dce559612c5b5682a6ae60ab
Carsten Senger <senger@rehfisch.de>
2018-01-13 21:53:07 +0100

    Run eslint --fix

--- davereader.test.js
+++ davereader.test.js
@@ -1,6 +1,6 @@
 const davereader = require('./davereader');
 
 test('Has davereader', () => {
-    myProductName = davereader.__get__('myProductName');
-    expect(myProductName).toBe('River5');
+	const myProductName = davereader.__get__('myProductName');
+	expect(myProductName).toBe('River5');
 });
